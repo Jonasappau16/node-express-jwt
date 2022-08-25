41255c1e2e735e211fab63648226c93a96f45905
J@y14 <jonasappau16@gmail.com>
2022-08-25 19:56:22 -0400

    update all

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const authRouters = require ('./routes/authRouters');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
@@ -12,13 +12,12 @@ app.use(express.json());
 app.set('view engine', 'ejs');
 
 // database connection
-// const dbURI = 'mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth';
-const dbURI = 'mongodb+srv://jwt-assignment:Maker1414@cluster0.rw0g3lz.mongodb.net/';
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+const dbURI = 'mongodb+srv://JWT:password1234@cluster0.hsszshn.mongodb.net/node-auth';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
   .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
 // routes
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
-app.use(authRouters);
+app.use(authRoutes);
