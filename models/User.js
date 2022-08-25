974af48127959020210197c204212dcb642c4c20
J@y14 <jonasappau16@gmail.com>
2022-08-24 22:15:31 -0400

    push all changes

--- models/User.js
+++ models/User.js
@@ -1,25 +1,34 @@
-const mongoose = require ('mongoose');
-const { isEmail } = require ('validator');
+const mongoose = require('mongoose');
+const { isEmail } = require('validator');
 
-// defining the schema .. (what user should look like in the database)
 const userSchema = new mongoose.Schema({
-    email: {
+    email:{
         type: String,
-        required: [true, 'please enter an email'],
+        required: [true, 'Please enter an email'],
         unique: true,
         lowercase: true,
-        validation: [ isEmail, 'Please enter a valid email']
+        validate: [ isEmail, 'Please enter a valid email']
     },
-    password: {
+    password:{
         type: String,
-        required: [true, 'please enter a password'],
+        required: [true, 'Please enter a password'],
         minlength: [6, 'Minimum password length is 6 characters']
     },
+})
+
+// fire a function after doc saved to DB
+userSchema.post('save', function(doc,next){
+    console.log('new user was created & save', doc);
+    next();
 });
 
-//creating model base on the schema
+//fire a function before doc saved to db
+
+userSchema.pre('save', function (next){
+    console.log('user about to be created @ saved', this);
+    next();
+})
+
 const User = mongoose.model('user', userSchema);
 
-//export module
-
-module.exports =User;
+module.exports = User;
